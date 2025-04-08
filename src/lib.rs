237fb78b2fce45d3b898701ebf7e464fc29b1a51
kz <kindlychung@gmail.com>
2025-04-08 09:34:30 +0200

    Voeg functies toe voor het verkrijgen van de huidige OS en architectuur, inclusief de mogelijkheid om alternatieve benamingen voor veelvoorkomende architecturen zoals x86_64 en aarch64 te erkennen, en zorg ervoor dat reguliere expressies op een consistente manier worden gemaakt.

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,22 @@ pub fn current_os_arch() -> Regex {
     let regex = format!("(?i)({}.*{}|{}.*{})", os, arch, arch, os);
     Regex::new(&regex).unwrap()
 }
+
+pub fn current_os() -> Regex {
+	let os = std::env::consts::OS;
+	let regex = format!("(?i)({})", os);
+	Regex::new(&regex).unwrap()
+}
+
+pub fn current_arch() -> Regex {
+	let arch = std::env::consts::ARCH;
+	let mut regex = format!("(?i)({})", arch);
+	if arch == "x86_64" {
+		regex = format!("(?i)({}|amd64)", arch);
+	} else if arch == "aarch64" {
+		regex = format!("(?i)({}|arm64)", arch);
+	} else if arch == "arm" {
+		regex = format!("(?i)({}|armv7)", arch);
+	}
+	Regex::new(&regex).unwrap()
+}
